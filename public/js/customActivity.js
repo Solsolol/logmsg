39a333f2ef94682579a53d188e85ca732edd8994
Solsolol <109537308+Solsolol@users.noreply.github.com>
2025-03-24 12:20:45 +0100

    Add files via upload

---
+++ public/js/customActivity.js
@@ -0,0 +1,104 @@
+define(['postmonger'], function(Postmonger) {
+	'use strict';
+	//var Postmonger = require('postmonger');
+	var connection = new Postmonger.Session();
+	var payload = {};
+	var steps = [
+		{'key': 'step1', 'label': 'Step 1'}
+	];
+
+	$(window).ready(function () {
+		connection.trigger('ready');
+	});
+
+    connection.on('clickedNext', save);
+    connection.on('requestedSchema', function(data) {
+    	console.log('*** Schema ***', JSON.stringify(data['schema']));
+    });
+
+    connection.on('initActivity', function(data) {
+    	console.log(data);
+    	initialize(data);
+    });
+
+    connection.on('requestedEndpoints', function(endpoints) {
+    	console.log(endpoints);
+    	connection.trigger('requestInteractionDefaults');
+    })
+
+    connection.on('requestedInteractionDefaults', function(settings) {
+    	console.log(settings);
+    	connection.trigger('requestTriggerEventDefinition');
+    });
+
+    connection.on('requestedTriggerEventDefinition', function(eventDefinitionModel) {
+    	console.log(eventDefinitionModel);
+    	connection.trigger('requestInteraction');
+    });
+
+    connection.on('requestedInteraction', function(interaction) {
+    	console.log(interaction);
+    });
+
+	function initialize(data) {
+		if (data) {
+			payload = data;
+		}
+		document.getElementById('testArea').value = JSON.stringify(data, null, 2);
+
+		var inArgs = payload["arguments"].execute.inArguments;
+
+		for(var i = 0; i < inArgs.length; i++) {
+			var inArg = inArgs[i];
+			console.log(inArg);
+			// there should only be one key saved per inArgument... (and this inArg key is based on the name of the field)
+			var inArgKey = Object.keys(inArg)[0];
+			//var inArgKey = inArgKeys[0];
+
+			if(document.getElementById(inArgKey)) document.getElementById(inArgKey).value = inArgs[i][inArgKey];
+		}
+		//var inputEls = document.getElementsByTagName('input');
+	}
+
+	function save() {
+		// first, clear any inArguments that was saved previously...
+		payload['arguments'].execute.inArguments = [];
+
+		// next, save each input field as an inArgument in the inArguments arr
+		var inputEls = document.getElementsByTagName('input');
+		// insert one argument into inArguments at a time
+		for(var i = 0; i < inputEls.length; i++) {
+			var inArg = {};
+			inArg[inputEls[i].id] = inputEls[i].value;
+			payload['arguments'].execute.inArguments.push(inArg)
+		}
+		payload["metaData"].isConfigured = true;
+		//var configuration = JSON.parse( document.getElementById('testArea').value );
+
+		console.log(payload);
+		var configuration = payload;
+
+		connection.trigger('updateActivity', configuration);
+	}
+
+	connection.trigger('requestSchema');
+
+	connection.trigger('requestInteractionDefaults');
+});
+
+var sendRequest = function() {
+	let request = new XMLHttpRequest();
+	request.open('POST', 'https://sfmcloggerwebapp.herokuapp.com/sendJson');
+	request.setRequestHeader("Content-Type", "application/json");
+
+	var jsonPayload = {"someTest":"abc"};
+	request.send(JSON.stringify(jsonPayload));
+	request.onload = () => {
+		console.log(request);
+		if(request.status === 200 || request.status === 201) {
+			console.log("It works!");
+		} else {
+			console.log(`error ${request.status} ${request.statusText}`);
+		}
+	};
+};
---
+++ routes/routes.js
@@ -0,0 +1,177 @@
+/****************************************************************************************************************
+ * File: routes.js																								*
+ * 																												*
+ * Desc: JS file that contains all of the necessary GET and POST calls.											*
+ * 
+ * *************************************************************************************************************/
+
+const routes = require('express').Router();
+const https = require('https');
+var fs = require('fs');
+const soap = require('soap');
+var configObj = JSON.parse(fs.readFileSync('./public/config.json', 'utf8'));
+//var icon = fs.readFileSync('./public/images/icon.png', 'utf8');
+
+// GET request to index.html
+routes.get('/', function(req, res, next) {
+	res.writeHead(200);
+	res.sendfile('index.html');
+});
+
+// GET request to config.json - used for Journey Builder to load configurations when using Custom Activity.
+routes.get('/config.json', function(req, res, next) {
+	res.status(200).json(configObj);
+});
+
+routes.get('/images/icon.png', function(req, res, next) {
+	//res.status(200).
+	fs.readFile('./public/images/icon.png', function(err, data) {
+		res.send(200, data);
+	});
+	// fails otherwise
+	res.send(400, icon);
+});
+
+routes.post('/execute', function(req, res, next) {
+	/*setTimeout(function() {
+		var reqPayload = req.body;
+		var inArgsReqPayload = reqPayload.inArguments;
+		var args = {};
+
+		// inArgs will always be an array of JSON objects of key-value pairs where values are from user-entered/DE fields
+		for(var i = 0; i < inArgsReqPayload.length; i++) {
+			var mc_val = inArgsReqPayload[i];
+
+			var mc_val_keys = Object.keys(mc_val);
+			// loop through the keys from the mc_val (there should only be one saved...)
+			if(mc_val_keys.length > 1 || mc_val_keys.length === 0) {
+				res.status(400).json({"error":"Bad Request. (Malformed data)"});
+				return;
+			} else {
+				if(mc_val_keys[0] !== "keyvalstatic" && mc_val_keys[0] !== "keyvaldynamic" && (args[mc_val_keys[0]] === null || args[mc_val_keys[0]] === undefined)) {
+					args[mc_val_keys[0]] = mc_val[mc_val_keys[0]];
+				}
+				if(mc_val_keys[0] === "keyvalstatic") {
+					if(args["keyvalpair"] === null || args["keyvalpair"] === undefined) {
+						args["keyvalpair"] = mc_val["keyvalstatic"];
+					} else {
+						args["keyvalpair"] += mc_val["keyvalstatic"];
+					}
+				}
+				if(mc_val_keys[0] === "keyvaldynamic") {
+					if(args["keyvalpair"] === null || args["keyvalpair"] === undefined) {
+						args["keyvalpair"] = mc_val["keyvaldynamic"];
+					} else {
+						args["keyvalpair"] += mc_val["keyvaldynamic"];
+					}
+				}
+			}
+		}
+
+		validateConfigurations(args, "/execute");
+		//validateConfigurations(req.body, "/execute");
+		var id = Math.floor(Math.random() * 1000);
+		//res.status(200).json({});
+
+		//res.status(201).json({"someExtraId":id});
+	}, 180000);*/
+	var reqPayload = req.body;
+	var inArgsReqPayload = reqPayload.inArguments;
+	var args = {};
+
+	// inArgs will always be an array of JSON objects of key-value pairs where values are from user-entered/DE fields
+	for(var i = 0; i < inArgsReqPayload.length; i++) {
+		var mc_val = inArgsReqPayload[i];
+
+		var mc_val_keys = Object.keys(mc_val);
+		// loop through the keys from the mc_val (there should only be one saved...)
+		if(mc_val_keys.length > 1 || mc_val_keys.length === 0) {
+			res.status(400).json({"error":"Bad Request. (Malformed data)"});
+			return;
+		} else {
+			if(mc_val_keys[0] !== "keyvalstatic" && mc_val_keys[0] !== "keyvaldynamic" && (args[mc_val_keys[0]] === null || args[mc_val_keys[0]] === undefined)) {
+				args[mc_val_keys[0]] = mc_val[mc_val_keys[0]];
+			}
+			if(mc_val_keys[0] === "keyvalstatic") {
+				if(args["keyvalpair"] === null || args["keyvalpair"] === undefined) {
+					args["keyvalpair"] = mc_val["keyvalstatic"];
+				} else {
+					args["keyvalpair"] += mc_val["keyvalstatic"];
+				}
+			}
+			if(mc_val_keys[0] === "keyvaldynamic") {
+				if(args["keyvalpair"] === null || args["keyvalpair"] === undefined) {
+					args["keyvalpair"] = mc_val["keyvaldynamic"];
+				} else {
+					args["keyvalpair"] += mc_val["keyvaldynamic"];
+				}
+			}
+		}
+	}
+
+	validateConfigurations(args, "/execute");
+	//validateConfigurations(req.body, "/execute");
+	var id = Math.floor(Math.random() * 1000);
+	//res.status(200).json({});
+
+	res.status(201).json({"someExtraId":id});
+	//res.status(400).end();
+	//res.status(400).json({"err":"This is a custom error."});
+});
+
+routes.post('/save', function(req, res, next) {
+	validateConfigurations(req.body, "/save");
+	//res.status(200).json({});
+	res.status(200).json({'activity':'Save'});
+});
+
+routes.post('/validate', function(req, res, next) {
+	validateConfigurations(req.body, "/validate");
+	//res.status(200).json({});
+	res.status(200).json({'activity':'Validate'});
+});
+
+routes.post('/stop', function(req, res, next) {
+	validateConfigurations(req.body, "/stop");
+	//res.status(200).json({});
+	res.status(200).json({'activity':'Stop'});
+});
+
+routes.post('/publish', function(req, res, next) {
+	validateConfigurations(req.body, "/publish");
+	//res.status(200).json({});
+	res.status(200).json({'activity':'Publish'});
+});
+
+routes.post('/sendJson', function(req, res, next) {
+	/*var url = 'https://mczl3f2-4hkhp1jhx9thdx6qljx8.soap.marketingcloudapis.com/etframework.wsdl';
+	var args = {};
+
+	soap.createClientAsync(url)
+		.then((client) => {
+			return client
+		})*/
+	validateConfigurations(req.body, "/sendJson");
+	res.status(200).json({'isSuccess':true});
+});
+
+routes.post('/processData', function(req, res, next) {
+	var id = Math.floor(Math.random() * 1000);
+	return res.status(200).json({"idVal":id});
+	//return res.status(400).json({"error":"invalid_request", "traceInfo":{"responseCode":"501", "errorDescription":"it's just an invalid request!!"}});
+});
+
+var validateConfigurations = function(requestPayload, pathEndpoint) {
+	const options = {
+	  hostname: "en118ofahjdyi.x.pipedream.net",
+	  port:443,
+	  path: pathEndpoint,
+	  method: "POST"
+	}
+
+	const req = https.request(options)
+	req.write(JSON.stringify(requestPayload));
+	req.end();
+};
+
+module.exports = routes;
