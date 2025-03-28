9fef3a5f497f2a80a5219e98914444bb038df5f8
Solsolol <109537308+Solsolol@users.noreply.github.com>
2025-03-28 11:19:19 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,21 +1,36 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+const cors = require('cors');
 const routes = require('./routes/routes.js');
 
 var app = express();
+
+// Middleware
+app.use(cors());
 app.use(express.static('public'));
+app.use(bodyParser.json());
+
 const port = process.env.PORT || 8080;
 
-app.use(bodyParser.json());
+// Routes
 app.use('/', routes);
 app.use('/icon.png', express.static('public/images/icon.png'));
 
-// GET request to web application, will always load up the web application
+// GET request to web application
 app.get('/', function(req, res) {
-	res.sendStatus(200);
-
+	res.sendFile(__dirname + '/public/index.html');
 });
 
+// Error handling middleware
+app.use(function(err, req, res, next) {
+	console.error(err.stack);
+	res.status(500).json({
+		error: 'Something broke!',
+		message: err.message
+	});
+});
+
+// Start server
 app.listen(port, () => {
-	console.log(`Listening on port #${port}`);
-})
+	console.log(`[SERVER] Application started on port ${port}`);
+});
